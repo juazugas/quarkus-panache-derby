ba052e02e5627207c15ce8e0f7abb4f4a541e65e
jzuriaga <jzuriaga@redhat.com>
2020-03-21 17:25:36 +0100

    Example working with DerbyDatabaseTestResource.

--- src/main/java/org/acme/FruitsResource.java
+++ src/main/java/org/acme/FruitsResource.java
@@ -1,12 +1,13 @@
 package org.acme;
 
 import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,17 +26,21 @@ public class FruitsResource {
         return Fruit.findAll().list();
     }
 
+    @Transactional
     @POST
     public Fruit create (Fruit fruit) {
         fruit.persist();
         return fruit;
     }
 
+    @Transactional
     @DELETE
     @Path("{id}")
-    public void delete (@PathParam("id") int id) {
-        Fruit.findByIdOptional(id)
-            .orElseThrow(NotFoundException::new).delete();
+    public void delete (@PathParam("id") long id) {
+
+        Fruit fruit = Fruit.<Fruit>findByIdOptional(id)
+                        .orElseThrow(NotFoundException::new);
+        fruit.delete();
     }
 
 }
--- src/test/java/org/acme/FruitsResourceTest.java
+++ src/test/java/org/acme/FruitsResourceTest.java
@@ -3,13 +3,15 @@ package org.acme;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.util.List;
-
+import javax.transaction.Transactional;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.derby.DerbyDatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -17,11 +19,34 @@ import org.junit.jupiter.api.Test;
 public class FruitsResourceTest {
 
     @Test
-    public void testmethodname() {
+    public void testListAll() {
+        Long count = Fruit.count();
         List<Fruit> result = given()
-            .when().get("/fruits")
+            .when().get("/fruit")
             .thenReturn().as(new TypeRef<List<Fruit>>() {});
-        assertThat(result, hasSize(3));
+        assertThat(result, hasSize(count.intValue()));
+    }
+
+    @Test
+    public void testCreate() {
+        given()
+            .when()
+            .body("{\"name\":\"FRUIT\"}")
+            .contentType(ContentType.JSON)
+            .post("/fruit")
+            .then()
+            .statusCode(200)
+            .body("name", equalTo("FRUIT"));
+    }
+
+    @Test
+    public void testDelete () {
+        Fruit fruit = Fruit.<Fruit>listAll().get(0);
+
+        given().when()
+            .delete("/fruit/"+fruit.id)
+            .then()
+            .statusCode(204);
     }
 
 }
